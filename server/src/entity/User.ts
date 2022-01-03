bee66741f3b93423f90b116c2f27d59c6f306ccd
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-03 06:26:23 +0000

    feat(chores): create mutation for login user (hashing password, tokenization)

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,10 +1,18 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+// ==================
+//   DATABASE Model
+// ==================
+
+@ObjectType() // initialize data-type
 @Entity("users")
 export class User extends BaseEntity {
+  @Field(() => Int) // expose the data (type of it)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field() // exposing the data (type of it)
   @Column("text")
   email: string;
 
--- server/src/user-resolvers.ts
+++ server/src/user-resolvers.ts
@@ -1,18 +1,40 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { hash } from "bcryptjs";
-import { User } from "./entity/User";
+import {
+  Arg,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { User } from './entity/User';
+
+@ObjectType()
+export class LoginResponse {
+  @Field()
+  accessToken: string;
+
+  @Field()
+  loggedIn: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
-    return "hi!";
+    return 'hi!';
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
   }
 
   @Mutation(() => Boolean)
   async register(
-    @Arg("email") email: string,
-    @Arg("password") password: string
+    @Arg('email') email: string,
+    @Arg('password') password: string
   ) {
     const hashedPassword = await hash(password, 12);
 
@@ -26,5 +48,41 @@ export class UserResolver {
       console.log(error);
       return false;
     }
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    // get user data based on email
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    // if there is no user return error
+    if (!user) {
+      throw new Error('invalid login credentials');
+    }
+
+    // compare the password ==> boolean
+    const valid = await compare(password, user.password);
+
+    // if the validation of password return false
+    if (!valid) {
+      throw new Error('invalid password! try again');
+    }
+
+    // log in successfully
+    return {
+      loggedIn: `successfully logged in as ${user.email}`,
+      accessToken: sign(
+        {
+          userId: user.id,
+        },
+        'envuzgvyymfy',
+        { expiresIn: '15m' }
+      ),
+    };
   }
 }
